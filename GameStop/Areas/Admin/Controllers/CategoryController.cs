bf4d94fd20ec384af3ddc452d5f78a672ac1a35f
namiqmamedov <namigmm@code.edu.az>
2023-02-16 15:45:40 +0400

    refactor: Updated category controller for area

--- GameStop/Areas/Admin/Controllers/CategoryController.cs
+++ GameStop/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStop.DAL;
+using GameStop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,37 @@ using System.Threading.Tasks;
 
 namespace GameStop.Areas.Admin.Controllers
 {
+    [Area("admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
