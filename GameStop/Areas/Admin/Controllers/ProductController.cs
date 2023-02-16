688b7de2c1ea135fbaed523f3f1074274cbde229
namiqmamedov <namigmm@code.edu.az>
2023-02-16 13:51:03 +0400

    refactor: Updated product controller Update section for area

--- GameStop/Areas/Admin/Controllers/ProductController.cs
+++ GameStop/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using GameStop.DAL;
+using GameStop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -22,9 +23,70 @@ namespace GameStop.Areas.Admin.Controllers
             return View(await _context.Products.ToListAsync());
         }
 
-        public async Task<IActionResult> Create()
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Product product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id,Product product)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            Product dbProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (dbProduct == null)
+            {
+                return NotFound();
+            }
+
+            dbProduct.Title = product.Title;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
